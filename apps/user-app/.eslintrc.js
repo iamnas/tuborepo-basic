e473a6352baa3449123b545fe0ff87f58d5f16dc
iamnas <iamnas.eth@gmail.com>
2024-09-13 23:19:15 +0530

    first commit

---
+++ apps/user-app/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["@repo/eslint-config/next.js",'plugin:tailwindcss/recommended'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+};
---
+++ packages/store/src/hook/useBalance.ts
@@ -0,0 +1,9 @@
+import {useRecoilValue} from "recoil";
+import { balanceAtom } from "../atoms/balance";
+
+export const useBalance=()=>{
+    const value = useRecoilValue(balanceAtom)
+    return value;
+}
+
+
